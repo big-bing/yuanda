740cd4c52b9a6fd3679f465f1479589dd92c86ec
bigbing <1075264520@qq.com>
2016-10-08 12:02:53 +0800

    fixed tag_list.html

--- app/models/pdf_utils/tag_pdf_util.rb
+++ app/models/pdf_utils/tag_pdf_util.rb
@@ -13,13 +13,13 @@ module PdfUtils::TagPdfUtil
     self.order_tags.each do |order_tag|
       order_tag.order.items.each do |item|
         item.number.times do |i|
-          tag_list << [order_tag.order.name, item.width, item.height]
+          tag_list << [order_tag.order.name, item.name, item.width, item.height, item.number]
         end
       end
     end
     list_html = "<tr class='tag_tr'>"
     tag_list.each_with_index do |tag, index|
-      list_html += "<td class='tag_td'>#{tag[0]}<br/>#{tag[1]}<br/>#{tag[2]}</td>"
+      list_html += "<td class='tag_td'>#{tag[0]}<br/>#{tag[1]}<br/>#{tag[2]}×#{tag[3]}×#{tag[4]}</td>"
       list_html += "</tr><tr class='tag_tr'>" if (index + 1) % 4 == 0
       list_html += "</tr>" if index + 1 == tag_list.size
     end
