936c1acaad25f0e49b4a6a0261e61e5e3ec7c24c
BigBing <1075264520@qq.com>
2018-01-13 17:19:24 +0800

    fix

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -37,12 +37,12 @@ PDFKit.configure do |config|
   # config.wkhtmltopdf = '/Users/bigbing/.rvm/gems/ruby-2.3.0/bin/wkhtmltopdf' # 'C:\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
   # config.wkhtmltopdf = 'D:\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
   # config.wkhtmltopdf = '/Users/bigbing/.rvm/gems/ruby-2.3.0/bin/wkhtmltopdf'
-  # config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
-  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  # config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
   config.default_options = {
-    :page_size => 'A4',
-    # :page_width => 210,
-    # :page_height => 297,
+    # :page_size => 'A4',
+    :page_width => 210,
+    :page_height => 300,
     :margin_top => 0,
     :margin_bottom => 0,
     :margin_left => 0,
